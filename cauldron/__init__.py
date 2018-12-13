129ff761e505991f9b5a99b47a345c2ca327d4b0
Dedeepya Bonthu <dedeepya.bonthu@1mg.com>
2018-12-13 16:26:03 +0530

    PostgreStore for 10.*

--- cauldron/__init__.py
+++ cauldron/__init__.py
@@ -1,5 +1,6 @@
 __all__ = ['PostgresStore', 'RedisCache', 'elasticsearch']
 
 from .sql import PostgresStore
+from .sql import PostgresStore10
 from .redis_cache import RedisCache
 from .es import  elasticsearch
