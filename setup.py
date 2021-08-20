f948e991dfd61158d2bbaba20d53450648925f86
himanshulakra1mg <himanshu.lakra@1mg.com>
2021-08-20 17:09:07 +0530

    fixed package versions in setup.py for vyked stability

--- setup.py
+++ setup.py
@@ -6,4 +6,10 @@ setup(name='cauldron',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/cauldron',
       description='Utils to reduce boilerplate code',
-      packages=['cauldron'], install_requires=['aiopg', 'aioredis', 'psycopg2','elasticsearch'])
+      packages=['cauldron'],
+      install_requires=[
+          'aiopg==0.16.0',
+          'aioredis==0.2.9',
+          'psycopg2==2.8.4',
+          'elasticsearch==7.6.0'
+      ])
